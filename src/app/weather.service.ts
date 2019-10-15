883b81b77c1c458f27bce20d54caed246cf506b0
James Hall <halljames631@gmail.com>
2019-10-15 11:14:51 -0400

    added url to enable cors

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,6 +9,6 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeather(){
-    return this.http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
+    return this.http.get('https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
   }
 }
