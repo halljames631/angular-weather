fbd13f4466eb6c611f64aebfcbf0a8433fdfd601
James Hall <halljames631@gmail.com>
2019-10-15 11:07:44 -0400

    added http client to get weather data

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeather(){
+    return this.http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
+  }
 }
